bbd3f53c1fded17fbe42425db2f9ba2c03f36acd
Derrek Gass <dwgass@gmail.com>
2019-12-19 12:36:01 -0800

    init debug

--- target/debug/deps/hello_world-b4a1df2ecc3a2289.d
+++ target/debug/deps/hello_world-b4a1df2ecc3a2289.d
@@ -1,5 +1,5 @@
-/Users/derrekgass/Documents/rust/hello_world/target/debug/deps/hello_world-b4a1df2ecc3a2289.rmeta: src/main.rs
+/Users/derrekgass/Documents/GitHub/hello_world/target/debug/deps/hello_world-b4a1df2ecc3a2289.rmeta: src/main.rs
 
-/Users/derrekgass/Documents/rust/hello_world/target/debug/deps/hello_world-b4a1df2ecc3a2289.d: src/main.rs
+/Users/derrekgass/Documents/GitHub/hello_world/target/debug/deps/hello_world-b4a1df2ecc3a2289.d: src/main.rs
 
 src/main.rs:
