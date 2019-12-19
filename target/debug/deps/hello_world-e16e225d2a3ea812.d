7f124fc573e38eeaf28243b496424f5e91028639
gassde <gassde@ecs.csus.edu>
2019-12-19 12:31:43 -0800

    init

---
+++ target/debug/deps/hello_world-e16e225d2a3ea812.d
@@ -0,0 +1,5 @@
+/Users/derrekgass/Documents/rust/hello_world/target/debug/deps/hello_world-e16e225d2a3ea812.rmeta: src/main.rs
+
+/Users/derrekgass/Documents/rust/hello_world/target/debug/deps/hello_world-e16e225d2a3ea812.d: src/main.rs
+
+src/main.rs:
